c9d2f5abe867c71c8473ed4f4aa6fa0f41250d88
Paul Hewlett <phewlett76@gmail.com>
2025-03-04 16:21:13 +0000

    Privatise tracing package
    AB#10464

--- azbus/tracing.go
+++ azbus/tracing.go
@@ -3,13 +3,71 @@ package azbus
 import (
 	"context"
 
+	"github.com/datatrails/go-datatrails-common/tracing"
 	opentracing "github.com/opentracing/opentracing-go"
+	otlog "github.com/opentracing/opentracing-go/log"
 )
 
-func (r *Receiver) CreateReceivedMessageTracingContext(ctx context.Context, message *ReceivedMessage, handler Handler) (context.Context, opentracing.Span) {
+type Spanner interface {
+	Open(context.Context, string, ...[2]string) context.Context
+	Close()
+	Log() Logger
+	InjectContext(context.Context) (map[string]string, error)
+}
+
+type spanning struct {
+	log  Logger
+	span tracing.Span
+}
+
+func NewSpanning(log Logger) *spanning {
+	return &spanning{log: log}
+}
+
+func (sp *spanning) Open(ctx context.Context, label string, fields ...[2]string) context.Context {
+
+	//log := sp.log.FromContext(ctx)
+	sp.span, ctx = tracing.StartSpanFromContext(ctx, label)
+
+	if len(fields) > 0 {
+		stringFields := make([]otlog.Field, len(fields))
+		for i := range fields {
+			stringFields[i] = otlog.String(fields[i][0], fields[i][1])
+		}
+		sp.span.LogFields(stringFields...)
+	}
+	return ctx
+}
+
+func (sp *spanning) Close() {
+	if sp.span != nil {
+		sp.span.Finish()
+		sp.span = nil
+	}
+	if sp.log != nil {
+		sp.log.Close()
+		sp.log = nil
+	}
+}
+
+func (sp *spanning) Log() Logger {
+	return sp.log
+}
+
+func (sp *spanning) InjectContext(ctx context.Context) (map[string]string, error) {
+	carrier := opentracing.TextMapCarrier{}
+	err := opentracing.GlobalTracer().Inject(sp.span.Context(), opentracing.TextMap, carrier)
+	if err != nil {
+		sp.log.Infof("updateSendingMesssage(): Unable to inject span context: %v", err)
+		return nil, err
+	}
+	return carrier, nil
+}
+
+func createReceivedMessageTracingContext(ctx context.Context, log Logger, message *ReceivedMessage, handler Handler) (context.Context, opentracing.Span) {
 	// We don't have the tracing span info on the context yet, that is what this function will add
-	// we we log using the reciever logger
-	r.log.Debugf("ContextFromReceivedMessage(): ApplicationProperties %v", message.ApplicationProperties)
+	// we log using the receiver logger
+	log.Debugf("ContextFromReceivedMessage(): ApplicationProperties %v", message.ApplicationProperties)
 
 	var opts = []opentracing.StartSpanOption{}
 	carrier := opentracing.TextMapCarrier{}
@@ -26,7 +84,7 @@ func (r *Receiver) CreateReceivedMessageTracingContext(ctx context.Context, mess
 	}
 	spanCtx, err := opentracing.GlobalTracer().Extract(opentracing.TextMap, carrier)
 	if err != nil {
-		r.log.Infof("CreateReceivedMessageWithTracingContext(): Unable to extract span context: %v", err)
+		log.Infof("CreateReceivedMessageWithTracingContext(): Unable to extract span context: %v", err)
 	} else {
 		opts = append(opts, opentracing.ChildOf(spanCtx))
 	}
@@ -35,24 +93,8 @@ func (r *Receiver) CreateReceivedMessageTracingContext(ctx context.Context, mess
 	return ctx, span
 }
 
-func (r *Receiver) handleReceivedMessageWithTracingContext(ctx context.Context, message *ReceivedMessage, handler Handler) (Disposition, context.Context, error) {
-	ctx, span := r.CreateReceivedMessageTracingContext(ctx, message, handler)
+func handleReceivedMessageWithTracingContext(ctx context.Context, log Logger, message *ReceivedMessage, handler Handler) (Disposition, context.Context, error) {
+	ctx, span := createReceivedMessageTracingContext(ctx, log, message, handler)
 	defer span.Finish()
 	return handler.Handle(ctx, message)
-}
-
-func (s *Sender) updateSendingMesssageForSpan(ctx context.Context, message *OutMessage, span opentracing.Span) {
-	log := s.log.FromContext(ctx)
-	defer log.Close()
-
-	carrier := opentracing.TextMapCarrier{}
-	err := opentracing.GlobalTracer().Inject(span.Context(), opentracing.TextMap, carrier)
-	if err != nil {
-		log.Infof("updateSendingMesssageForSpan(): Unable to inject span context: %v", err)
-		return
-	}
-	for k, v := range carrier {
-		OutMessageSetProperty(message, k, v)
-	}
-	log.Debugf("updateSendingMesssageForSpan(): ApplicationProperties %v", OutMessageProperties(message))
 }
